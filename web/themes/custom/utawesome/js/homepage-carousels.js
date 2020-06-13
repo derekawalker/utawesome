e082f46daa3e0da7f0a83c81007fe408216ece23
Derek Walker <johndoe@example.com>
2020-06-13 00:34:23 -0600

    gear

--- web/themes/custom/utawesome/js/homepage-carousels.js
+++ web/themes/custom/utawesome/js/homepage-carousels.js
@@ -2,7 +2,6 @@
   'use strict';
   $(window).on('load', function () {
     $('.owl-me .view-content').addClass('owl-carousel');
-    $('.owl-me-gear .view-content').addClass('owl-carousel-gear');
 
     $('.owl-carousel').owlCarousel({
       loop: true,
@@ -22,28 +21,6 @@
         },
         1340: {
           items: 5.5,
-        },
-      },
-    });
-
-    $('.owl-carousel-gear').owlCarousel({
-      loop: true,
-      margin: 0,
-      nav: false,
-      dots: false,
-      responsiveClass: true,
-      responsive: {
-        0: {
-          items: 2.5,
-        },
-        480: {
-          items: 3.5,
-        },
-        1024: {
-          items: 5.5,
-        },
-        1340: {
-          items: 6.5,
         },
       },
     });
