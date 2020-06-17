41bbc993a2b21445e5663e085a5407dcb4c7c67a
Derek Walker <johndoe@example.com>
2020-06-17 10:01:39 -0600

    nav search

--- web/themes/custom/utawesome/js/views.js
+++ web/themes/custom/utawesome/js/views.js
@@ -1,25 +1,26 @@
 (function ($, Drupal, drupalSettings) {
   'use strict';
 
-  Drupal.behaviors.common = {
-    attach: function (context) {
-      // Remove TID's onload.
-      Drupal.common.remove_tid();
+  // Drupal.behaviors.common = {
+  //   attach: function (context) {
+  //     // Remove TID's onload.
+  //     Drupal.common.remove_tid();
 
-      // Remove TID's onchange.
-      $('body')
-        .find('.form-autocomplete')
-        .on('autocompleteclose', function () {
-          Drupal.common.remove_tid();
-        });
-    },
-  };
+  //     // Remove TID's onchange.
+  //     $('body')
+  //       .find('.form-autocomplete')
+  //       .on('autocompleteclose', function () {
+  //         Drupal.common.remove_tid();
+  //       });
+  //   },
+  // };
 
-  Drupal.common = {
-    remove_tid: function () {
-      let field_autocomplete = $('body').find('.form-autocomplete');
-      let autocomplete_tid_stripped = field_autocomplete.val().replace(/ *\([^)]*\) */g, '');
-      field_autocomplete.val(autocomplete_tid_stripped);
-    },
-  };
+  // Drupal.common = {
+  //   remove_tid: function () {
+  //     let field_autocomplete = $('body').find('.form-autocomplete');
+  //     let autocomplete_tid_stripped = field_autocomplete.val().replace(/ *\([^)]*\) */g, '');
+  //     field_autocomplete.val(autocomplete_tid_stripped);
+  //   },
+  // };
+
 })(jQuery, Drupal, drupalSettings);
